67f96d56e6cdc2b207463f45a7b9fda1e6146043
Thomas Rutzer <thomas.rutzer@artcom.de>
2019-12-11 21:07:28 +0100

    Enable high accuracy of Geolocation

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -7,7 +7,7 @@ class Geolocation {
     watchID = navigator.geolocation.watchPosition((position) => {
       const distance = getDistance(lat, long, position.coords.latitude, position.coords.longitude)
       cb(distance)
-    })
+    }, null, { enableHighAccuracy: true})
   }
 
   static clear() {
