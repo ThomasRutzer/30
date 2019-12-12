0f8e7874ab57de972f14339b46fb2d0661033756
Thomas Rutzer <thomas.rutzer@artcom.de>
2019-12-12 19:26:15 +0100

    Add high accuracy to Geolocation

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -3,13 +3,17 @@ import getDistance from "./getDistance"
 let watchID = null
 
 class Geolocation {
-  static async intersect(lat, long, cb) {
+  static intersect(lat, long, cb) {
+    if (watchID) {
+      this.clear()
+    }
+
     watchID = navigator.geolocation.watchPosition((position) => {
       const distance = getDistance(lat, long, position.coords.latitude, position.coords.longitude)
       cb(distance)
     }, 
-    (err) => console.log(err), 
-    { enableHighAccuracy: true, maximumAge: 0})
+    err => console.log(err), 
+    { enableHighAccuracy: true })
   }
 
   static clear() {
