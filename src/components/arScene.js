6366653f06a5fbde8296a039cfa4fd02983fc46f
Thomas Rutzer <thomas.rutzer@artcom.de>
2019-12-16 09:33:53 +0100

    New Model

--- src/components/arScene.js
+++ src/components/arScene.js
@@ -16,10 +16,14 @@ class ArScene extends LitElement {
         arjs="debugUIEnabled: false" 
         vr-mode-ui="enabled: false">
         <a-assets>
-          <a-asset-item id="eiffel" material="color: #5d48a3;" src="assets/eiffel_tower.gltf">
+          <a-asset-item id="eiffel-obj" src="assets/eiffel_tower.obj"></a-asset-item>
+          <a-asset-item id="eiffel-mtl" src="assets/eiffel_tower.mtl"></a-asset-item>      
         </a-assets>
         <a-marker type="pattern" url="assets/marker_30.patt">
-          <a-gltf-model scale="0.005 0.005 0.005" src="#eiffel"></a-gltf-model>
+          <a-entity 
+          scale="0.02 0.02 0.02" 
+          material="src: assets/eiffel_texture.png" 
+          obj-model="obj: #eiffel-obj"></a-entity>
         </a-marker>
         <a-entity camera></a-entity>
       </a-scene>
